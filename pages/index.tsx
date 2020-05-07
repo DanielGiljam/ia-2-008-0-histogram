6a4711956bfae610ad246d3489b64a40d5a36f0a
Ivan Akkuratov <jojowoodo@yahoo.com>
2020-05-07 14:54:53 +0300

    adds initial svg capabilities ( A red rectangle)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,38 @@
+import React, { useRef, useEffect } from 'react'
+import { select } from 'd3-selection'
+
+const Index: React.FC = () => {
+  const svgRef = useRef<SVGSVGElement | null>(null)
+  useEffect(() => {
+
+    select(svgRef.current)
+      .append('rect')
+      .attr('width', 100)
+      .attr('height', 100)
+      .attr('fill', 'red')
+  })
+  return (
+    <div>
+      <svg ref={svgRef} />
+      <line />
+
+    </div>
+  )
+}
+
+//const Index = (): JSX.Element => <div><h1>Bildens pixelfördelning</h1></div>
+
+
+
+
+
+export default Index
+
+/*
 function Index(): JSX.Element {
+
   return <div>
     <h1>Bildens pixelfördelning</h1>
 
   </div>
-
-}
-
-export default Index
+}*/
