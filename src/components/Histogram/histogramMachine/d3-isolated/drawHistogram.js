fbaae5e0493560f9cb9cd613ad58e09056c27cd0
Daniel Giljam <33825175+DanielGiljam@users.noreply.github.com>
2020-05-26 11:20:11 +0300

    Fixes drawHistogram function

--- src/components/Histogram/histogramMachine/d3-isolated/drawHistogram.js
+++ src/components/Histogram/histogramMachine/d3-isolated/drawHistogram.js
@@ -1,70 +1,52 @@
 import * as d3 from "d3"
 
 function drawHistogram (data, state) {
-  console.log(data.shift())
+  const margins = {left: 36, bottom: 20}
+  const rect = document.getElementById("histogram").getBoundingClientRect()
 
-  const {
-    margin = {top: 20, right: 30, bottom: 30, left: 60},
-    height,
-    width,
-  } = document.getElementById("histogram").getBoundingClientRect()
+  const height = rect.height - margins.bottom
+  const width = rect.width - margins.left
+
   const svg = d3
     .select("#histogram")
     .append("svg")
     .attr("width", "100%")
     .attr("height", "100%")
     .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    .attr("transform", `translate(${margins.left},0)`)
 
+  data.shift()
+
+  const r = []
+  const g = []
+  const b = []
+  const l = []
+
+  for (const {red, green, blue} of data) {
+    r.push(red)
+    g.push(green)
+    b.push(blue)
+    l.push((red + green + blue) / 3)
+  }
+
+  const yScale = d3
+    .scaleLinear()
+    .domain([
+      d3.min([d3.min(r), d3.min(g), d3.min(b), d3.min(l)]),
+      d3.max([d3.max(r), d3.max(g), d3.max(b), d3.max(l)]),
+    ])
+    .range([height, 0])
+  const xScale = d3.scaleLinear().domain([0, data.length]).range([0, width])
   const area = d3
     .area()
     .x(function (d, i) {
-      return (i * width) / r.length
+      return (i * width) / data.length
     })
     .y0(height)
     .y1(function (d) {
       return yScale(d)
     })
-  // Skapar arrays separat för varje rgb värde
-  const r = []
-  const g = []
-  const b = []
-  const a = []
-  const rgbValues = []
 
-  let o = 0
-  let j = 0
-  let k = 0
-  const m = 0
-
-  // Far igenom varje data rgb värde och sätter in dem i respektive arrays
-  for (const red of data) {
-    // console.log(data[i].red)
-    r.push(data[o].red)
-    o++
-  }
-  for (const green of data) {
-    // console.log(data[j].green)
-    g.push(data[j].green)
-    j++
-  }
-  for (const blue of data) {
-    // console.log(data[k].blue)
-    b.push(data[k].blue)
-    k++
-  }
-  rgbValues.push(r + g + b)
-
-  // console.log(r)
-
-  // Skapar scale efter r (som är en array av data.red)
-  const yScale = d3
-    .scaleLinear()
-    .domain([d3.min(r), d3.max([d3.max(r), d3.max(g), d3.max(b)])])
-    .range([height, 0])
-
-  const xScale = d3.scaleLinear().domain([0, data.length]).range([0, width])
-  // Axis
   const yAxis = d3.axisLeft(yScale)
   const xAxis = d3.axisBottom(xScale)
 
@@ -87,12 +69,7 @@ function drawHistogram (data, state) {
     .attr("d", area(b))
     .attr("id", "blue")
 
-  // rita axisarna
   svg.append("g").call(yAxis)
-
-  svg
-    .append("g")
-    .call(xAxis)
-    .attr("transform", "translate( 2, " + height + ")")
+  svg.append("g").call(xAxis).attr("transform", `translate(0,${height})`)
 }
 export default drawHistogram
